b8e43dd723223e04b1f83697578db28b2075555f
chuh007 <chhuh007@gmail.com>
2025-02-28 01:49:04 +0900

    fix

---
+++ Winter_GameMake_1/Assets/Code/Core/Managers/TitleManager.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
